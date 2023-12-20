2be841958ed941e39b05c46abaa47d3abbb016c8
rama-can <heyramacan@gmail.com>
2023-12-20 17:32:42 +0700

    Add landing page controller & route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Frontend\LandingController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,9 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('pages.frontend.landing');
-});
+Route::get('/', LandingController::class)->name('landing');
 
 // Route::get('/dashboard', function () {
 //     return view('dashboard');
