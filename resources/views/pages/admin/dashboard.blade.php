fe785b4236ca3c86da0de86608565beab391fd72
rama-can <heyramacan@gmail.com>
2023-12-21 22:52:14 +0700

    Add greeting welcome for admin

--- resources/views/pages/admin/dashboard.blade.php
+++ resources/views/pages/admin/dashboard.blade.php
@@ -6,9 +6,28 @@
     </x-slot>
     <div class="py-12">
         <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
-            <div class="grid gap-6 mb-8 md:grid-cols-2 xl:grid-cols-4">
+            <div class="bg-gradient-to-r from-slate-200 to-indigo-600 dark:from-slate-800 dark:to-indigo-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <div class="relative">
+                        @php
+                            date_default_timezone_set('Asia/Jakarta');
+                            $hour = date('H');
+                            if ($hour >= 6 && $hour < 12) {
+                                $text = __('Good Morning');
+                            } elseif ($hour >= 12 && $hour < 18) {
+                                $text = __('Good Evening');
+                            } else {
+                                $text = __('Good Night');
+                            }
+                        @endphp
+                        <h1 class="text-2xl md:text-3xl text-slate-800 dark:text-slate-100 font-bold mb-1">{{ $text }}, {{ Auth::user()->name }} 👋</h1>
+                        <p class="dark:text-indigo-200">Here is what's happening with your projects today:</p>
+                    </div>
+                </div>
+            </div>
+            <div class="grid gap-6 mb-8 md:grid-cols-2 xl:grid-cols-4 mt-4">
                 <!-- Card -->
-                <div class="flex items-center p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800">
+                <div class="flex items-center p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800 border dark:border-slate-600">
                     <div class="p-3 mr-4 text-orange-500 bg-orange-100 rounded-full dark:text-orange-100 dark:bg-orange-500">
                         <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
                             <path d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z">
@@ -25,7 +44,7 @@
                     </div>
                 </div>
                 <!-- Card -->
-                <div class="flex items-center p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800">
+                <div class="flex items-center p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800 border dark:border-slate-600">
                     <div class="p-3 mr-4 text-teal-500 bg-teal-100 rounded-full dark:text-teal-100 dark:bg-teal-500">
                         <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
                             <path fill-rule="evenodd" d="M18 5v8a2 2 0 01-2 2h-5l-5 4v-4H4a2 2 0 01-2-2V5a2 2 0 012-2h12a2 2 0 012 2zM7 8H5v2h2V8zm2 0h2v2H9V8zm6 0h-2v2h2V8z" clip-rule="evenodd"></path>
@@ -41,7 +60,7 @@
                     </div>
                 </div>
                 <!-- Card -->
-                <div class="flex items-center p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800">
+                <div class="flex items-center p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800 border dark:border-slate-600">
                     <div class="p-3 mr-4 text-green-500 bg-green-100 rounded-full dark:text-green-100 dark:bg-green-500">
                         <x-tabler-tools-kitchen-2 class="w-6 h-6" />
                     </div>
@@ -55,7 +74,7 @@
                     </div>
                 </div>
                 <!-- Card -->
-                <div class="flex items-center p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800">
+                <div class="flex items-center p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800 border dark:border-slate-600">
                     <div class="p-3 mr-4 text-blue-500 bg-blue-100 rounded-full dark:text-blue-100 dark:bg-blue-500">
                         <x-tabler-category class="w-6 h-6" />
                     </div>
@@ -67,12 +86,6 @@
                             {{ $category }}
                         </p>
                     </div>
-                </div>
-            </div>
-
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
                 </div>
             </div>
         </div>
